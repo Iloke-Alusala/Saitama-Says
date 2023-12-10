6e6245a994b762ea8aef31e4a04b0fa64efb580c
Iloke-Alusala <93942530+Iloke-Alusala@users.noreply.github.com>
2023-12-10 07:57:24 +0200

    Add files via upload

---
+++ lib/Colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  final Blue3 = const Color(0xFF487AB1);
+  final Blue2 = const Color(0xFF4DA1D2);
+  final Blue1 = const Color(0xFF42CBC7);
+  final coolWhite = const Color(0x72FFFFFF);
+  final BlueCard2 = const Color(0xFF7C9EF2);
+  final C1 = const Color(0xFFDDF2F3);
+  final C2 = const Color(0xFFEEF5D4);
+  final C3 = const Color(0xFFFDEDBA);
+  final C4 = const Color(0xFFF7F0EA);
+  final List<Color> neonColours = const [Color(0xFF83C6F1),
+      Color(0xFFDDF2F3),
+    Color(0xFFEEF5D4),
+    Color(0xFFFDEDBA),
+    Color(0xFFF7F0EA),
+    Color(0xFFC9D5FF),
+    Color(0xFFE8B586),
+    Color(0xFFE586A6),
+    Color(0xFFCE94E8),
+    Color(0xFF9AECDD),
+    Color(0xFFB4E798),
+  ];
+  const AppColors();
+}
