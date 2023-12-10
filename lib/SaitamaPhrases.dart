8c793cd2ec5be274025923deba6423da48093cc0
Iloke-Alusala <93942530+Iloke-Alusala@users.noreply.github.com>
2023-12-10 07:53:32 +0200

    Add files via upload

---
+++ lib/SaitamaPhrases.dart
@@ -0,0 +1,29 @@
+class SaitamaLines{
+  final List<String> lines = const[
+    'Hey there, looking for some quotes',
+    "I'm telling you - the guy was a complete stranger, just walked up and gave me the bag.",
+    "I hope that what I've said hasn't hurt you too much.",
+    "I'm not really surprised that you murdered him.",
+    "Give me one good reason why I should wear a dress.",
+    "If you leave now, you get nothing.",
+    "You're too good for this world.",
+    "Are you taking his side against me?",
+    "If we both stick to the story, they can't prove anything.",
+    "The foodbank was sold out when I got there.",
+    "Well, this is where I live.",
+    "You were meant to be watching him!",
+    "I don't think I could live alone again.",
+    "You must be mad, coming here like this.",
+    "I did a pregnancy test.",
+    "No! I'm tired of doing what you say.",
+    "A boy needs his father.",
+    "Did you enjoy yourself last night?",
+    "You don't want to live in a society like this, yet you don't want to do anything about it!",
+    "Midnight, on the bridge. Come alone.",
+    "You have his eyes.",
+    "Are you kidding me? We're not 'fine'!",
+    "Oh yeah, you really told him, didn't you?",
+    "You don't think that was just lemonade in your glass, do you?"
+  ];
+  const SaitamaLines();
+}
